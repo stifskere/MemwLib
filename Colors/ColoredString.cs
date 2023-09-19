9100272d7091b3bba05ec276f5305324a47b045e
stifskere <stifskere@gmail.com>
2023-09-19 20:25:14 +0200

    dev: Colored string format idea

--- Colors/ColoredString.cs
+++ Colors/ColoredString.cs
@@ -1,13 +1,14 @@
+using System.Text.RegularExpressions;
 using JetBrains.Annotations;
 using MemwLib.Colors.Types;
 
 namespace MemwLib.Colors;
 
-public static class ColoredString
+public static partial class ColoredString
 {
     [PublicAPI]
     public static string SetForeground(this string handle, int first, int last, Color color)
-        => SetForeground(handle, first..last, color);
+        => handle.SetForeground(first..last, color);
     
     [PublicAPI]
     public static string SetForeground(this string handle, Range range, Color color) 
@@ -15,7 +16,7 @@ public static class ColoredString
 
     [PublicAPI]
     public static string SetBackground(this string handle, int first, int last, Color color)
-        => SetBackground(handle, first..last, color);
+        => handle.SetBackground(first..last, color);
 
     [PublicAPI]
     public static string SetBackground(this string handle, Range range, Color color) 
@@ -24,10 +25,17 @@ public static class ColoredString
     private static string InsertHandle(string handle, Range range, Color color, byte type)
     {
         string ansiPrefix = $"\x1b[{type:D3};2;{color.R:D3};{color.G:D3};{color.B:D3}m";
-
+        string ansiPostfix = "\x1b[0m";
+        
         for (int i = 0; i < handle.Length; i++)
         {
             
         }
     }
+
+    [GeneratedRegex(@"(\x1b\[\d{3};2;\d{3};\d{3};\d{3}m)")]
+    private static partial Regex AnsiPrefixRegex();
+    
+    [GeneratedRegex(@"(\x1b\[0m)")]
+    private static partial Regex AnsiPostfixRegex();
 }
