0807f108bd9b2fb24691a1140c9ebca647c3a63a
stifskere <stifskere@gmail.com>
2023-09-19 14:40:19 +0200

    dev: ColoredString

--- Colors/ColoredString.cs
+++ Colors/ColoredString.cs
@@ -1,11 +1,33 @@
-namespace MemwLib.Ansi;
+using JetBrains.Annotations;
+using MemwLib.Colors.Types;
 
-public class ColoredString
+namespace MemwLib.Colors;
+
+public static class ColoredString
 {
-    private string Handle;
+    [PublicAPI]
+    public static string SetForeground(this string handle, int first, int last, Color color)
+        => SetForeground(handle, first..last, color);
     
-    public ColoredString(string handle = "")
+    [PublicAPI]
+    public static string SetForeground(this string handle, Range range, Color color) 
+        => InsertHandle(handle, range, color, 38);
+
+    [PublicAPI]
+    public static string SetBackground(this string handle, int first, int last, Color color)
+        => SetBackground(handle, first..last, color);
+
+    [PublicAPI]
+    public static string SetBackground(this string handle, Range range, Color color) 
+        => InsertHandle(handle, range, color, 48);
+
+    private static string InsertHandle(string handle, Range range, Color color, byte type)
     {
-        Handle = handle;
+        string ansiPrefix = $"\x1b[{type:D3};2;{color.R:D3};{color.G:D3};{color.B:D3}m";
+
+        for (int i = 0; i < handle.Length; i++)
+        {
+            
+        }
     }
 }
