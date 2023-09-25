328e46564be66a26db918369bd8f587bf6e0c2f4
stifskere <stifskere@gmail.com>
2023-09-25 15:01:29 +0200

    dev: progress

---
+++ Http/HttpClient.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using MemwLib.Http.Types;
+using MemwLib.Http.Types.Entities;
+
+namespace MemwLib.Http;
+
+public static class HttpClient
+{
+    [PublicAPI]
+    public static ResponseEntity Request(RequestMethodType type, string uri)
+    {
+        
+    }
+}
---
+++ Http/Types/Uri.cs
@@ -0,0 +1,9 @@
+namespace MemwLib.Http.Types;
+
+public class Uri
+{
+    public Uri(string content)
+    {
+        
+    }
+}
